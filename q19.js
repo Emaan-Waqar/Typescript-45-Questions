20787e1ef45683b7173ec6af93aa86a354781b79
Emaan-Waqar <Emaanwaqar0102@gmail.com>
2023-11-19 20:46:08 +0500

    add question 19 and 20

---
+++ q19.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+// of people you are inviting to dinner.
+let guest = ["Albert einstein", "cristiano Ronaldo", "Leonel messi"];
+// for (let i=0;i<guest.length;i+=1){
+//     console.log(`hello,${guest[i]} you are invited to dinner`);
+// }
+console.log("we are inviting " + guest.length + " people");
