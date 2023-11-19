017cf7e2d51a7cdc2cd9645db8ebd2a7eab64633
Emaan-Waqar <Emaanwaqar0102@gmail.com>
2023-11-19 20:58:50 +0500

    add question

---
+++ q21.ts
@@ -0,0 +1,7 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let age={
+    name:"ahmed",
+    age:"5",
+    isMale:true
+}
+console.log(` name is :${age.name} and age is:${age.age}`);
